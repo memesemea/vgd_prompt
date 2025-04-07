6105e060d79b36a8abbb2421ecda43fc9ce72120
memesemea <OseremeA931@stemcollegiate.ca>
2025-04-07 13:58:51 -0600

    Create Apr07.cs
    fnIPAEhf'ogruhigaUOvgaEOhgipeaoghaEKUgfj'hoefkjhaepigjhaUjfaebyiufjhAEo9fijaEOIEfopjaeipfp/hoiAjgoagfheAU>f

---
+++ Apr07.cs
@@ -0,0 +1,9 @@
+Console.WriteLine("Generating invoices for customer \"Contoso Corp\" ... \n");
+Console.WriteLine("Invoice: 1021\t\tComplete!");
+Console.WriteLine("Invoice: 1022\t\tComplete!");
+Console.WriteLine("\nOutput Directory:\t");
+Console.WriteLine(@"    c:\source\repos    
+        (this is where your code goes)");
+        Console.WriteLine("\u3053\u3093\u306B\u3061\u306F World!");
+
+//I learned how to use backslashes for escape sequences
