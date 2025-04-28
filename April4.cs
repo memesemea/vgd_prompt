62b53ab4f28964be149a4be7b1785af1f7e454c9
memesemea <OseremeA931@stemcollegiate.ca>
2025-04-28 15:21:55 -0600

    Create April4.cs
    vgrrffeefeaEFFGGegfgrefgfegfee

---
+++ April4.cs
@@ -0,0 +1,20 @@
+int firstNumber = 12;
+int secondNumber = 7;
+Console.WriteLine(firstNumber + secondNumber);
+
+string firstName = "Bob";
+int widgetsSold = 7;
+Console.WriteLine(firstName + " sold " + (widgetsSold + 7) + " widgets.");
+
+//I learned how to do math like operations and add things together
+
+Console.WriteLine($"Modulus of 200 / 5 : {200 % 5}");
+Console.WriteLine($"Modulus of 7 / 5 : {7 % 5}");
+
+int value1 = 3 + 4 * 5;
+int value2 = (3 + 4) * 5;
+Console.WriteLine(value1);
+Console.WriteLine(value2);
+
+//I learned how to perform different math operations and do BEDMAS 
+
